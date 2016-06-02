e7e63b7da8f0ad76b51cd3642bbe2652be28d88e
DarioMC <dmcsubotai95@gmail.com>
2016-06-01 19:22:02 -0600

    Cuarto avance interfaz

--- EDCervezas/registrarcerveza.cpp
+++ EDCervezas/registrarcerveza.cpp
@@ -7,6 +7,19 @@ RegistrarCerveza::RegistrarCerveza(QWidget *parent) :
     ui(new Ui::RegistrarCerveza)
 {
     ui->setupUi(this);
+    ui->comboBoxFamilia->addItem("ALE");
+    ui->comboBoxFamilia->addItem("Lager");
+    ui->comboBoxFamilia->addItem("Lambic");
+
+
+    ui->comboBoxEstilo->addItem("Británico");
+    ui->comboBoxEstilo->addItem("Belga");
+    ui->comboBoxEstilo->addItem("Alemán");
+    ui->comboBoxEstilo->addItem("Americana");
+    ui->comboBoxEstilo->addItem("Brown");
+    ui->comboBoxEstilo->addItem("Porter/Stout");
+    ui->comboBoxEstilo->addItem("Otros");
+
 }
 
 RegistrarCerveza::~RegistrarCerveza()
