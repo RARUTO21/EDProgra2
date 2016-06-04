ebcb8eb4a2b16103cb4ee7ae48c55aceb8ae3907
Alexis <ameyitfabian@gmail.com>
2016-06-03 21:24:08 -0600

    Añadí la clase MatrizAdyacencia al proyecto de aerolíneas

---
+++ EDAerolineas/matrizadyacencia.cpp
@@ -0,0 +1,6 @@
+#include "matrizadyacencia.h"
+
+MatrizAdyacencia::MatrizAdyacencia()
+{
+
+}
---
+++ EDAerolineas/matrizadyacencia.h
@@ -0,0 +1,11 @@
+#ifndef MATRIZADYACENCIA_H
+#define MATRIZADYACENCIA_H
+
+
+class MatrizAdyacencia
+{
+public:
+    MatrizAdyacencia();
+};
+
+#endif // MATRIZADYACENCIA_H
